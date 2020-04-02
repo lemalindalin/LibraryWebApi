9a4291cb84f5cfb751a6096eb1fb98c97bc035b1
Franck <lemalindalin@gmail.com>
2020-04-02 19:36:19 -0400

    Initial commit

---
+++ Controllers/BookController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryWebApi.Controllers
+{
+    [Route("api/book/v1")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private readonly 
+    }
+}
