9f13e66ff70bdffd065e1f883da835dd1cf50954
Franck <lemalindalin@gmail.com>
2020-04-02 20:04:06 -0400

    1st commit

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LibrairiesBackend;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,6 +12,71 @@ namespace LibraryWebApi.Controllers
     [ApiController]
     public class BookController : ControllerBase
     {
-        private readonly 
+        private readonly BookLogic _logic;
+
+        public BookController()
+        {
+            var repo = new EFGenericRepository<BookPoco>();
+            _logic = new BookLogic(repo);
+        }
+
+        [HttpGet]
+        [Route("book/{id}")]
+        public ActionResult GetBook(Guid id)
+        {
+            BookPoco poco = _logic.Get(id);
+            if (poco == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(poco);
+            }
+        }
+
+        [HttpGet]
+        [Route("book")]
+        public ActionResult GetAllBook()
+        {
+            var books = _logic.GetAll();
+            if (books == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(books);
+            }
+        }
+
+
+        [HttpPost]
+        [Route("book")]
+        public ActionResult PostBook(
+            [FromBody] BookPoco[] appBookPocos)
+        {
+            _logic.Add(appBookPocos);
+            return Ok();
+        }
+
+        [HttpPut]
+        [Route("book")]
+
+        public ActionResult PutBook(
+            [FromBody] BookPoco[] pocos)
+        {
+            _logic.Update(pocos);
+            return Ok();
+        }
+
+        [HttpDelete]
+        [Route("book")]
+        public ActionResult DeleteBook(
+            [FromBody] BookPoco[] pocos)
+        {
+            _logic.Delete(pocos);
+            return Ok();
+        }
     }
 }
--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -11,5 +11,6 @@ namespace LibraryWebApi.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
+
     }
 }
