a8ca52139ba7ac4090b8914ff87af7ee72941194
Franck <lemalindalin@gmail.com>
2020-04-02 20:09:57 -0400

    2nd commit

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LibrairiesBackend;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,6 +12,71 @@ namespace LibraryWebApi.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
+        private readonly UserLogic _logic;
 
+        public UserController()
+        {
+            var repo = new EFGenericRepository<UserPoco>();
+            _logic = new UserLogic(repo);
+        }
+
+        [HttpGet]
+        [Route("user/{id}")]
+        public ActionResult GetUser(Guid id)
+        {
+            UserPoco poco = _logic.Get(id);
+            if (poco == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(poco);
+            }
+        }
+
+        [HttpGet]
+        [Route("user")]
+        public ActionResult GetAllUser()
+        {
+            var books = _logic.GetAll();
+            if (books == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(books);
+            }
+        }
+
+
+        [HttpPost]
+        [Route("user")]
+        public ActionResult PostUser(
+            [FromBody] UserPoco[] appBookPocos)
+        {
+            _logic.Add(appBookPocos);
+            return Ok();
+        }
+
+        [HttpPut]
+        [Route("user")]
+
+        public ActionResult PutUser(
+            [FromBody] UserPoco[] pocos)
+        {
+            _logic.Update(pocos);
+            return Ok();
+        }
+
+        [HttpDelete]
+        [Route("user")]
+        public ActionResult DeleteUser(
+            [FromBody] UserPoco[] pocos)
+        {
+            _logic.Delete(pocos);
+            return Ok();
+        }
     }
 }
